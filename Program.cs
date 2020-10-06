debb5e642b45d6e705d349da57c528722b187bd4
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-07 02:42:48 +0530

    [kirti]Add. Add Last Name

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LastName.validateFirstName();
+          LastName.validateLastName();
+        }
+    }
+}
