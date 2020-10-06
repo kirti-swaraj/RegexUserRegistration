0cb6cb0cf2e6e9c81307155d7dc1cfd0128f6dcb
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-07 02:55:10 +0530

    [kirti]Add . Password Rule 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rule2.validateFirstName();
+            Rule2.validateLastName();
+            Rule2.validateEmail();
+            Rule2.validatePhoneNo();
+            Rule2.validatePassword();
+
+        }
+    }
+}
+
