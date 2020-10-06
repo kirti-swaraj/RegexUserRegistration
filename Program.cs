e665b8adb34226b15722dc0acf692a07c280b582
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-07 02:52:57 +0530

    [kirti]Add. Password Rule 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rule1.validateFirstName();
+            Rule1.validateLastName();
+            Rule1.validateEmail();
+            Rule1.validatePhoneNo();
+            Rule1.validatePassword();
+
+        }
+    }
+}
+
