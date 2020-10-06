7ec2c6b82be844810036f42819a0d8f219c180c2
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-07 02:57:59 +0530

    [kirti]Add . Rule 3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rule3.validateFirstName();
+            Rule3.validateLastName();
+            Rule3.validateEmail();
+            Rule3.validatePhoneNo();
+            Rule3.validatePassword();
+
+        }
+    }
+}
+
