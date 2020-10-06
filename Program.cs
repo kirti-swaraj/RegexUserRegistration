fdadb2af93f272b7e557928f651a286a6b264d54
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-07 03:00:19 +0530

    [kirti]Add . Password Rule 4

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rule4.validateFirstName();
+            Rule4.validateLastName();
+            Rule4.validateEmail();
+            Rule4.validatePhoneNo();
+            Rule4.validatePassword();
+
+        }
+    }
+}
+
